34ef38206467a4d59b9b01b6a698bc0071bbcf7f
abousoro67 <abousoro67@gmail.com>
2025-02-28 11:57:08 +0000

    Add files via upload
    create my first projet

---
+++ projetgomycode/design/cript.js
@@ -0,0 +1,7 @@
+document.getElementById("showFormBtn").addEventListener("click", function() {
+    document.getElementById("contactForm").style.display = "block";
+});
+
+document.getElementById("closeFormBtn").addEventListener("click", function() {
+    document.getElementById("contactForm").style.display = "none";
+});
